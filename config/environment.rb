cb63e56ac4833720760ed4a5645659eb7f5b2b79
Peter Barnes <barnespeter213@gmail.com>
2014-03-30 14:56:15 -0500

    Finish heroku email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 GrApp::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["SG_USERNAME"],
+  :password => ENV["SG_PASSWORD"],
+  :domain => 'groom.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
